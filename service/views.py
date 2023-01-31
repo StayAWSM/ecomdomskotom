4ad4eaee88ee42b067cc60a0764c8efeae206e86
Raphael <raphael.mukha@gmail.com>
2023-01-31 16:51:59 +0300

    first views

--- service/views.py
+++ service/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.http import HttpResponse
+from django.core.mail import send_mail
 
-# Create your views here.
+from .forms import ContactForm
+from .models import Contact
+
+
+class ContactCreate(CreateView):
+    model = Contact
+    # fields = ['name', 'email', 'message']
+    success_url = reverse_lazy('home_page')
+    form_class = ContactForm
+    template_name = 'service/contact_form.html'
+
+
+def home_page(request):
+    return HttpResponse('<h1>Home_page</h1>')
+
+
