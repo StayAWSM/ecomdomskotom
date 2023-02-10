4e023f1fe502ce75f7d7c0e8b57bfa832350efc3
Vladimir <hvatit5minut@gmail.com>
2023-02-10 19:54:57 +0300

    changing the design to isinstance

--- service/utils.py
+++ service/utils.py
@@ -4,17 +4,17 @@ def likes(names):
 
     WARNING: Operator match-case works with Python3.10+
     """
-    if isinstance(type(names), list):
+    if not isinstance(type(names), list):
         raise TypeError('The function accepts only an array')
-
-    match len(names):
-        case 0:
-            return 'No one likes this'
-        case 1:
-            return f'{names[0]} likes this'
-        case 2:
-            return f'{names[0]} and {names[1]} like this'
-        case 3:
-            return f'{names[0]}, {names[1]} and {names[2]} like this'
-        case _:
-            return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
+    else:
+        match len(names):
+            case 0:
+                return 'No one likes this'
+            case 1:
+                return f'{names[0]} likes this'
+            case 2:
+                return f'{names[0]} and {names[1]} like this'
+            case 3:
+                return f'{names[0]}, {names[1]} and {names[2]} like this'
+            case _:
+                return f'{names[0]}, {names[1]} and {len(names) - 2} others like this'
