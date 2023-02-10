e7041a5ceebc78b13c57028999770ead5673767e
Vladimir <hvatit5minut@gmail.com>
2023-02-10 19:43:15 +0300

    changing the design to isinstance

--- service/utils.py
+++ service/utils.py
@@ -4,7 +4,7 @@ def likes(names):
 
     WARNING: Operator match-case works with Python3.10+
     """
-    if type(names) != list:
+    if isinstance(type(names), list):
         raise TypeError('The function accepts only an array')
 
     match len(names):
