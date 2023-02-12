8398dab261981c6b993f56bd4b8f9ffa5e2fbabc
Raphael <raphael.mukha@gmail.com>
2023-02-12 13:58:25 +0300

    Some refactoring for lint.

--- service/tests/test_utils.py
+++ service/tests/test_utils.py
@@ -8,12 +8,14 @@ class TestLikes(unittest.TestCase):
         self.assertEqual(likes([]), 'No one likes this')
         self.assertEqual(likes(['Bob']), 'Bob likes this')
         self.assertEqual(likes(['Alex', 'Bob']), 'Alex and Bob like this')
-        self.assertEqual(likes(['Artur', 'Bob', 'John']), 'Artur, Bob and John like this')
-        self.assertEqual(likes(['Artur', 'Bob', 'John', 'Liza']), 'Artur, Bob and 2 others like this')
+        self.assertEqual(likes(['Artur', 'Bob', 'John']),
+                         'Artur, Bob and John like this')
+        self.assertEqual(likes(['Artur', 'Bob', 'John', 'Liza']),
+                         'Artur, Bob and 2 others like this')
 
     def test_likes_exceptions(self):
         self.assertRaises(TypeError, likes, 1)
         self.assertRaises(TypeError, likes, 45.7)
         self.assertRaises(TypeError, likes, {'Alex': 1})
         self.assertRaises(TypeError, likes, ('Bob', ))
-        self.assertRaises(TypeError, likes, {'Alex', 'Bob', 'Alex'})
+        self.assertRaises(TypeError, likes, {'Alex', 'Bob', 'Nikolay'})
