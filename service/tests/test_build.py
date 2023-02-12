fb9c7b2373e3ee295468476aa34de958d127102a
Raphael <raphael.mukha@gmail.com>
2023-02-12 11:19:42 +0300

    Testing tests for CI .gitlab-ci.yml

---
+++ service/tests/test_build.py
@@ -0,0 +1,13 @@
+import unittest
+
+
+class TestBuild(unittest.TestCase):
+
+    def test_sum(self):
+        self.assertEqual(1+1, 2)
+
+    def test_sum1(self):
+        self.assertEqual(1+1, 2)
+
+    def test_sum2(self):
+        self.assertEqual(1+1, 2)
