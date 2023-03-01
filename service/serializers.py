38fde77167b2c75cb4fb0e8537324019462ca684
Vladimir <hvatit5minut@gmail.com>
2023-03-01 16:46:59 +0300

    Small corrections in the fields

--- service/serializers.py
+++ service/serializers.py
@@ -5,4 +5,13 @@ from .models import Booking
 class BookingSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Booking
-        fields = '__all__'
+        fields = [
+            'name',
+            'phone_number',
+            'guest_number',
+            'comment',
+            'date',
+            'time_from',
+            'time_to',
+            'table_number',
+        ]
