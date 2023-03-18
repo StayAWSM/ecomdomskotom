81c755c0087aef8e7869a457e98e2d9db779cd8c
MikkiPro <mik0898@mail.com>
2023-03-18 22:15:33 +0300

    задачи на 4 уроке

---
+++ service/homework/homework4_lessan/dz11.py
@@ -0,0 +1,12 @@
+user = int(input())
+if user <= 13:
+    print('детство')
+if user >= 14  >= 24:
+    print('молодость')
+if user >= 25 <= 59:
+    print('зрелость')
+if user >= 60:
+    print('старость')
+
+
+
---
+++ service/homework/homework4_lessan/dz12.py
@@ -0,0 +1,6 @@
+numbers = int(input())
+if numbers >= -1 >= 17:
+    print('принадлежит')
+if numbers >= 17:
+    print('не принадлежит')
+
