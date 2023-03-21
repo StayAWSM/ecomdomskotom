4dfd81f39d889b85ab87f8c4da7dd51c5783ba75
MikkiPro <mik0898@mail.com>
2023-03-21 16:20:10 +0300

    задачи на 4 уроке

--- service/homework/lessaon6/dz18.py
+++ service/homework/lessaon6/dz18.py
@@ -1,10 +1,15 @@
-a = int(input())
+a = int(input('месяц'))
+b = int(input('дата'))
 
-if a == 12:
-    print('31 day')
 
-if a == 5:
+if a == '1 3 5 7 8 10 11':
     print('31 day')
 
 if a == 2:
     print('28 day')
+
+if a == '4, 6, 10, 11':
+    print('30 day')
+else:
+    print('ошибка')
+#пока не привильно нодумаю как исправить
